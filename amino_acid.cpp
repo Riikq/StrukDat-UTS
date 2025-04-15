a8d7077c6e9f6d0bd447d89e8e6c9417b4bcebbe
Riikq <tariqahmad0510@gmail.com>
2025-04-15 13:44:43 +0700

    menggunakan struct

--- amino_acid.cpp
+++ amino_acid.cpp
@@ -1,73 +1,90 @@
 #include <iostream>
+#include <vector>
 #include <unordered_map>
 #include <string>
 using namespace std;
 
-// Fungsi untuk mengubah DNA ke RNA
-string dnaToRna(const string& dna) {
-    string rna = dna;
-    for (char& base : rna) {
-        if (base == 'T') base = 'U';
-    }
-    return rna;
-}
+// Struct untuk satu triplet DNA
+struct DNATriplet {
+    string triplet;
 
-// Fungsi untuk mendapatkan nama asam amino dari kodon RNA
-string getAminoAcid(const string& codon) {
-    unordered_map<string, string> codonTable = {
-        {"UUU", "Phenylalanine"}, {"UUC", "Phenylalanine"},
-        {"UUA", "Leucine"}, {"UUG", "Leucine"},
-        {"CUU", "Leucine"}, {"CUC", "Leucine"}, {"CUA", "Leucine"}, {"CUG", "Leucine"},
-        {"AUU", "Isoleucine"}, {"AUC", "Isoleucine"}, {"AUA", "Isoleucine"},
-        {"AUG", "Methionine"}, // Start codon
-        {"GUU", "Valine"}, {"GUC", "Valine"}, {"GUA", "Valine"}, {"GUG", "Valine"},
-        {"UCU", "Serine"}, {"UCC", "Serine"}, {"UCA", "Serine"}, {"UCG", "Serine"},
-        {"CCU", "Proline"}, {"CCC", "Proline"}, {"CCA", "Proline"}, {"CCG", "Proline"},
-        {"ACU", "Threonine"}, {"ACC", "Threonine"}, {"ACA", "Threonine"}, {"ACG", "Threonine"},
-        {"GCU", "Alanine"}, {"GCC", "Alanine"}, {"GCA", "Alanine"}, {"GCG", "Alanine"},
-        {"UAU", "Tyrosine"}, {"UAC", "Tyrosine"},
-        {"UAA", "Stop"}, {"UAG", "Stop"}, {"UGA", "Stop"},
-        {"CAU", "Histidine"}, {"CAC", "Histidine"},
-        {"CAA", "Glutamine"}, {"CAG", "Glutamine"},
-        {"AAU", "Asparagine"}, {"AAC", "Asparagine"},
-        {"AAA", "Lysine"}, {"AAG", "Lysine"},
-        {"GAU", "Aspartic acid"}, {"GAC", "Aspartic acid"},
-        {"GAA", "Glutamic acid"}, {"GAG", "Glutamic acid"},
-        {"UGU", "Cysteine"}, {"UGC", "Cysteine"},
-        {"UGG", "Tryptophan"},
-        {"CGU", "Arginine"}, {"CGC", "Arginine"}, {"CGA", "Arginine"}, {"CGG", "Arginine"},
-        {"AGU", "Serine"}, {"AGC", "Serine"},
-        {"AGA", "Arginine"}, {"AGG", "Arginine"},
-        {"GGU", "Glycine"}, {"GGC", "Glycine"}, {"GGA", "Glycine"}, {"GGG", "Glycine"}
-    };
-
-    auto it = codonTable.find(codon);
-    if (it != codonTable.end()) {
-        return it->second;
-    } else {
-        return "Invalid codon";
+    // Konversi ke RNA
+    string toRNA() const {
+        string rna = triplet;
+        for (char& base : rna) {
+            if (base == 'T') base = 'U';
+        }
+        return rna;
     }
-}
+
+    // Dapatkan asam amino dari kodon RNA
+    string getAminoAcid() const {
+        static unordered_map<string, string> codonTable = {
+            {"UUU", "Phenylalanine"}, {"UUC", "Phenylalanine"},
+            {"UUA", "Leucine"}, {"UUG", "Leucine"},
+            {"CUU", "Leucine"}, {"CUC", "Leucine"}, {"CUA", "Leucine"}, {"CUG", "Leucine"},
+            {"AUU", "Isoleucine"}, {"AUC", "Isoleucine"}, {"AUA", "Isoleucine"},
+            {"AUG", "Methionine"},
+            {"GUU", "Valine"}, {"GUC", "Valine"}, {"GUA", "Valine"}, {"GUG", "Valine"},
+            {"UCU", "Serine"}, {"UCC", "Serine"}, {"UCA", "Serine"}, {"UCG", "Serine"},
+            {"CCU", "Proline"}, {"CCC", "Proline"}, {"CCA", "Proline"}, {"CCG", "Proline"},
+            {"ACU", "Threonine"}, {"ACC", "Threonine"}, {"ACA", "Threonine"}, {"ACG", "Threonine"},
+            {"GCU", "Alanine"}, {"GCC", "Alanine"}, {"GCA", "Alanine"}, {"GCG", "Alanine"},
+            {"UAU", "Tyrosine"}, {"UAC", "Tyrosine"},
+            {"UAA", "Stop"}, {"UAG", "Stop"}, {"UGA", "Stop"},
+            {"CAU", "Histidine"}, {"CAC", "Histidine"},
+            {"CAA", "Glutamine"}, {"CAG", "Glutamine"},
+            {"AAU", "Asparagine"}, {"AAC", "Asparagine"},
+            {"AAA", "Lysine"}, {"AAG", "Lysine"},
+            {"GAU", "Aspartic acid"}, {"GAC", "Aspartic acid"},
+            {"GAA", "Glutamic acid"}, {"GAG", "Glutamic acid"},
+            {"UGU", "Cysteine"}, {"UGC", "Cysteine"},
+            {"UGG", "Tryptophan"},
+            {"CGU", "Arginine"}, {"CGC", "Arginine"}, {"CGA", "Arginine"}, {"CGG", "Arginine"},
+            {"AGU", "Serine"}, {"AGC", "Serine"},
+            {"AGA", "Arginine"}, {"AGG", "Arginine"},
+            {"GGU", "Glycine"}, {"GGC", "Glycine"}, {"GGA", "Glycine"}, {"GGG", "Glycine"}
+        };
+
+        string rna = toRNA();
+        auto it = codonTable.find(rna);
+        if (it != codonTable.end()) {
+            return it->second;
+        } else {
+            return "Invalid";
+        }
+    }
+};
 
 int main() {
-    string dnaTriplet;
-    cout << "Masukkan triplet DNA (contoh: ATG): ";
-    cin >> dnaTriplet;
+    string dnaStrand;
+    cout << "Masukkan rantai DNA (panjang kelipatan 3, contoh: ATGCGTACG): ";
+    cin >> dnaStrand;
 
-    // Ubah ke huruf kapital semua
-    for (char& c : dnaTriplet) c = toupper(c);
+    // Ubah semua huruf jadi kapital
+    for (char& c : dnaStrand) c = toupper(c);
 
-    // Validasi input
-    if (dnaTriplet.length() != 3) {
-        cout << "Triplet harus terdiri dari 3 huruf." << endl;
+    if (dnaStrand.length() % 3 != 0) {
+        cout << "Rantai DNA harus kelipatan 3!" << endl;
         return 1;
     }
 
-    string rnaCodon = dnaToRna(dnaTriplet);
-    string aminoAcid = getAminoAcid(rnaCodon);
+    vector<DNATriplet> triplets;
+    for (size_t i = 0; i < dnaStrand.length(); i += 3) {
+        string codon = dnaStrand.substr(i, 3);
+        triplets.push_back({codon});
+    }
 
-    cout << "RNA kodon: " << rnaCodon << endl;
-    cout << "Asam amino: " << aminoAcid << endl;
+    cout << "\nHasil terjemahan DNA ke asam amino:\n";
+    for (const auto& triplet : triplets) {
+        string rna = triplet.toRNA();
+        string amino = triplet.getAminoAcid();
+
+        cout << triplet.triplet << " (RNA: " << rna << ") → " << amino << endl;
+
+        // Hentikan jika asam amino = Stop
+        if (amino == "Stop") break;
+    }
 
     return 0;
 }
